4d55d932d340b12576860d8267f4aaea83d6673d
ksenia-b <ksenia.barshchyk@gmail.com>
2024-09-05 11:54:47 +0300

    added provider for react-redux

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      main file
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client'
+import { store } from './store/store';
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+
+)
